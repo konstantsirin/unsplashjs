8b622591f8cfb745127c941bbcbf9c38af98a5a7
konstantsirin <konstantin.sirin@mail.ru>
2020-06-03 10:48:07 +0500

    Update PhotoDetail.js

--- src/components/PhotoDetailContainer/PhotoDetail/PhotoDetail.js
+++ src/components/PhotoDetailContainer/PhotoDetail/PhotoDetail.js
@@ -41,7 +41,9 @@ function PhotoDetail(props){
                                 </button>
                             </header>
                             <button className={styles.photoDetailClose} onClick={() => {window.history.back(); enableScroll();}}>X</button>
-                            <img onClick={() => {window.history.back(); enableScroll();}} className={styles.photoDetailImage} src={currentPhoto ? currentPhoto.urls.regular : null} alt="Изображение" />
+                            <span>
+                                <img onClick={() => {window.history.back(); enableScroll();}} className={styles.photoDetailImage} src={currentPhoto ? currentPhoto.urls.regular : null} alt="Изображение" />
+                            </span>
                             <footer className={styles.photoDetailFooter}>
                                 <div onClick={userProfileLink}>
                                     <span><img className={styles.fotoProfilePrev} src={currentPhoto ? currentPhoto.user.profile_image.small : noUser} alt="Фото профиля"/></span>
