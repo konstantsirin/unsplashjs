67b7f3493fd1ad8f36f65df540ea30167c128702
konstantsirin <konstantin.sirin@mail.ru>
2020-06-18 15:38:03 +0500

    Update constants.js

--- src/global/constants/constants.js
+++ src/global/constants/constants.js
@@ -2,5 +2,5 @@ export const SET_PHOTO = 'SET_PHOTO';
 export const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 export const TOGGLE_IS_PHOTO_DETAIL_STATUS = 'TOGGLE_IS_PHOTO_DETAIL_STATUS';
 export const TOGGLE_LIKE = 'TOGGLE_LIKE';
-export const URL = "http://localhost:3000/";
+export const URL = "http://web-dev-page.ru/";
 export const PER_PAGE = 10;
