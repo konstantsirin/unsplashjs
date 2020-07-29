1cdb685e1f57f14d440fbbecea749ce8e834c8e1
konstantsirin <konstantin.sirin@mail.ru>
2020-07-29 20:53:51 +0500

    fix 29-07-2020

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,8 @@
 export const QUANTITY_REQUIRED_PHOTO = 10;
 export const ACCESS_KEY = 'DhN9G_bo-rHP6T8JurB-S_-OQvjFCrnszggu9gUs5fo';
 export const SECRET = 'JdFTk0TyyKE4tZL1viY2zqQcV9iy7MqHwysKlfVdEKs';
-export const URL = 'http://web-dev-page.ru/';
+
+export const URL = ( typeof process.env.NODE_ENV !==  'undefined' ) ? 'http://localhost:3000/' : 'http://web-dev-page.ru/';
 
 export const PERMISSIONS = [
     'public',
